a595950b0966663770e748712c8d303d2b6b9eb2
Andrey Krasman <andreaskrasman@yandex.ru>
2022-01-17 20:40:35 +0700

    first review

--- scripts/index.js
+++ scripts/index.js
@@ -8,11 +8,16 @@ let popupNameInput = document.querySelector('.popup__input-name')
 let popupCareerInput = document.querySelector('.popup__input-career')
 
 const popupSaveButton = document.querySelector('.popup__save-button')
+let formElement = document.querySelector('.popup__form')
+
+popupNameInput.setAttribute('value', profileName.textContent)
+popupCareerInput.setAttribute('value', profileCareer.textContent)
+
+// popupNameInput.value = profileName.textContent
+// popupCareerInput.value = profileCareer.textContent
 
 function popupOpen() {
     popup.classList.add('popup_opened')
-    popupNameInput.setAttribute('value', profileName.textContent)
-    popupCareerInput.setAttribute('value', profileCareer.textContent)
 }
 
 function popupClose() {
@@ -21,12 +26,23 @@ function popupClose() {
     popupCareerInput.value = profileCareer.textContent
 }
 
-function saveChange() {
-    popup.classList.remove('popup_opened')
+function formSubmitHandler() {
     profileName.textContent = popupNameInput.value
     profileCareer.textContent = popupCareerInput.value
+    popup.classList.remove('popup_opened')
 }
 
 popupOpenButton.addEventListener('click', popupOpen)
 popupCloseButton.addEventListener('click', popupClose)
-popupSaveButton.addEventListener('click', saveChange)
+popupSaveButton.addEventListener('click', formSubmitHandler)
+
+// let formElement = document.querySelector('.popup__form');
+// let nameInput = document.querySelector('.popup__user-name')
+// let jobInput = document.querySelector('.popup__user-job')
+// function formSubmitHandler(evt) {
+//   evt.preventDefault();
+//   document.querySelector('.profile__job').textContent = jobInput.value;
+//   document.querySelector('.profile__name').textContent = nameInput.value;
+//   closePopup()
+// }
+// formElement.addEventListener('submit', formSubmitHandler);
