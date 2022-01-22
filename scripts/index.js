fcae1613808452a0f28ef3a9e00d65084211fbb1
Andrey Krasman <andreaskrasman@yandex.ru>
2022-01-22 23:03:05 +0700

    secRev

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,27 @@
-const popup = document.querySelector('.popup')
 const popupOpenButton = document.querySelector('.profile__edit-button')
+const popup = document.querySelector('.popup')
 const popupCloseButton = document.querySelector('.popup__close-button')
 
 let profileName = document.querySelector('.profile__name')
 let profileCareer = document.querySelector('.profile__career')
-let popupNameInput = document.querySelector('.popup__input-name')
-let popupCareerInput = document.querySelector('.popup__input-career')
 
-const popupSaveButton = document.querySelector('.popup__save-button')
+let popupNameInput = document.querySelector('#popupInputName')
+let popupCareerInput = document.querySelector('#popupInputCareer')
+
 let formElement = document.querySelector('.popup__form')
-
-popupNameInput.setAttribute('value', profileName.textContent)
-popupCareerInput.setAttribute('value', profileCareer.textContent)
 
 function popupOpen() {
     popup.classList.add('popup_opened')
-}
-
-function popupClose() {
-    popup.classList.remove('popup_opened')
     popupNameInput.value = profileName.textContent
     popupCareerInput.value = profileCareer.textContent
 }
 
-function formSubmitHandler() {
+function popupClose() {
+    popup.classList.remove('popup_opened')
+}
+
+function formSubmitHandler(event) {
+    event.preventDefault(event)
     profileName.textContent = popupNameInput.value
     profileCareer.textContent = popupCareerInput.value
     popup.classList.remove('popup_opened')
@@ -31,4 +29,4 @@ function formSubmitHandler() {
 
 popupOpenButton.addEventListener('click', popupOpen)
 popupCloseButton.addEventListener('click', popupClose)
-popupSaveButton.addEventListener('click', formSubmitHandler)
+formElement.addEventListener('submit', formSubmitHandler)
