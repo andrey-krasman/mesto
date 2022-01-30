74955d0f502e8e9b64eb63c7d0ccadcad83f9080
Andrey Krasman <andreaskrasman@yandex.ru>
2022-01-30 15:02:03 +0700

    кривое добавление карточек реализовано

---
+++ scripts/data.js
@@ -0,0 +1,33 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+      alt: 'Горы',
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+      alt: 'Снежное озеро',
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+      alt: 'Спальный район',
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+      alt: 'Спящий вулкан',
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+      alt: 'Железная дорога',
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+      alt: 'Окрестности Байкала',
+    }
+  ]; 
+ 
--- scripts/index.js
+++ scripts/index.js
@@ -1,100 +1,116 @@
-const popupOpenButton = document.querySelector('.profile__edit-button')
-const popup = document.querySelector('.popup')
-const popupCloseButton = document.querySelector('.popup__close-button')
+const popupEditOpenButton = document.querySelector('.profile__edit-button');
+const popupAddPlaceOpenButton = document.querySelector('.profile__add-button');
 
-let profileName = document.querySelector('.profile__name')
-let profileCareer = document.querySelector('.profile__career')
+const popupEditProfile = document.querySelector('.popup__edit-profile');
+const popupAddPlace = document.querySelector('.popup__add-place');
 
-let popupNameInput = document.querySelector('#popupInputName')
-let popupCareerInput = document.querySelector('#popupInputCareer')
+const popupCloseEditProfileButton = document.querySelector('.popup__edit-profile .popup__close-button');
+const popupCloseAddPlaceButton = document.querySelector('.popup__add-place .popup__close-button');
 
-let formElement = document.querySelector('.popup__form')
+const popup = document.querySelector('.popup');
 
-function popupOpen() {
-    popup.classList.add('popup_opened')
-    popupNameInput.value = profileName.textContent
-    popupCareerInput.value = profileCareer.textContent
+const elementsSection = document.querySelector('.elements');
+
+const elementTemplate = document.querySelector('#oneElement').content;
+
+let profileName = document.querySelector('.profile__name');
+let profileCareer = document.querySelector('.profile__career');
+
+let popupNameInput = document.querySelector('#popupInputName');
+let popupCareerInput = document.querySelector('#popupInputCareer');
+let popupPlaceNameInput = document.querySelector('#popupInputAddPlaceName');
+let popupPlaceLinkInput = document.querySelector('#popupInputAddPlaceLink');
+
+
+let formEditElement = document.querySelector('.popup__edit-profile .popup__form');
+let formAddPlace = document.querySelector('.popup__add-place .popup__form');
+
+function popupEditOpen() {
+    popupEditProfile.classList.add('popup_opened');
+    popupNameInput.value = profileName.textContent;
+    popupCareerInput.value = profileCareer.textContent;
 }
 
-function popupClose() {
-    popup.classList.remove('popup_opened')
+function popupAddPlaceOpen() {
+  popupAddPlace.classList.add('popup_opened');
+  popupPlaceNameInput.value = ''
+  popupPlaceLinkInput.value = ''
 }
 
-function formSubmitHandler(event) {
-    event.preventDefault(event)
-    profileName.textContent = popupNameInput.value
-    profileCareer.textContent = popupCareerInput.value
-    popup.classList.remove('popup_opened')
+function popupEditProfileClose() {
+  popupEditProfile.classList.remove('popup_opened');
 }
 
-popupOpenButton.addEventListener('click', popupOpen)
-popupCloseButton.addEventListener('click', popupClose)
-formElement.addEventListener('submit', formSubmitHandler)
+function popupAddPlaceClose() {
+  popupAddPlace.classList.remove('popup_opened');
+  popupPlaceNameInput.removeAttribute.value
+  popupPlaceLinkInput.removeAttribute.value
+}
 
+function formEditProfileSubmitHandler(event) {
+    event.preventDefault(event);
+    profileName.textContent = popupNameInput.value;
+    profileCareer.textContent = popupCareerInput.value;
+    popupEditProfile.classList.remove('popup_opened');
+}
+
+
+initialCards.forEach (function(initialCards) {
+  //clone element
+  const oneElement = elementTemplate.querySelector('.elements__element').cloneNode(true);
+  //add to content
+  oneElement.querySelector('.elements__image').src = initialCards.link;
+  oneElement.querySelector('.elements__name').textContent = initialCards.name;
+  oneElement.querySelector('.elements__image').alt = initialCards.alt;
+  //get container for content
+  //add content to containers end
+  elementsSection.append(oneElement);
+})
+
+function formAddPlaceSubmitHandler(event) {
+  event.preventDefault(event);
+  let newPlaceForAdd = {
+        name: popupPlaceNameInput.value,
+        link: popupPlaceLinkInput.value,
+        alt: 'Новое место',
+      }
+      
+  const newPlace = elementTemplate.querySelector('.elements__element').cloneNode(true);
+
+  newPlace.querySelector('.elements__image').src = newPlaceForAdd.link;
+  newPlace.querySelector('.elements__name').textContent = newPlaceForAdd.name;
+  newPlace.querySelector('.elements__image').alt = newPlaceForAdd.alt;
+  initialCards.unshift(newPlaceForAdd)
+  elementsSection.prepend(newPlace)
+  // initialCards.pop()
+  popupAddPlace.classList.remove('popup_opened');
+}
+
+popupEditOpenButton.addEventListener('click', popupEditOpen);
+popupAddPlaceOpenButton.addEventListener('click', popupAddPlaceOpen);
+
+popupCloseEditProfileButton.addEventListener('click', popupEditProfileClose);
+popupCloseAddPlaceButton.addEventListener('click', popupAddPlaceClose)
+
+formEditElement.addEventListener('submit', formEditProfileSubmitHandler);
+formAddPlace.addEventListener('submit', formAddPlaceSubmitHandler);
 
 // ADD FOTO TO THE ELEMENT
 
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; 
- 
-initialCards.forEach (function(initialCards) {
-    //find template.content
-    const elementTemplate = document.querySelector('#oneElement').content;
-    //clone element
-    const oneElement = elementTemplate.querySelector('.elements__element').cloneNode(true);
-    //add to content
-    oneElement.querySelector('.elements__image').src = initialCards.link;
-    oneElement.querySelector('.elements__name').textContent = initialCards.name;
-    //get container for content
-    const elementsSection = document.querySelector('.elements');
-    //add content to containers end
-    elementsSection.append(oneElement)
-})
+
+// initialCards.forEach (function(initialCards) {
+//     //find template.content
+//     const elementTemplate = document.querySelector('#oneElement').content;
+//     //clone element
+//     const oneElement = elementTemplate.querySelector('.elements__element').cloneNode(true);
+//     //add to content
+//     oneElement.querySelector('.elements__image').src = initialCards.link;
+//     oneElement.querySelector('.elements__name').textContent = initialCards.name;
+//     oneElement.querySelector('.elements__image').alt = initialCards.alt;
+//     //get container for content
+//     const elementsSection = document.querySelector('.elements');
+//     //add content to containers end
+//     elementsSection.append(oneElement);
+// })
 
 
-// CREATE A NEW POPUP: ADD A NEW PLACE
-
-const popups= [
-    {
-      title: 'Редактировать профиль',
-      formName: 'profile',
-      inputUpperId: 'popupInputName',
-      inputLowerId: 'popupInputCareer',
-      inputUpperName: 'name',
-      inputLowerName: 'career',
-      buttonText: 'Сохранить',
-    },
-    {
-        title: 'Новое место',
-        formName: 'addNewPlace',
-        inputUpperId: 'popupInputNamePlace',
-        inputLowerId: 'popupInputLinkPlace',
-        inputUpperName: 'newPlace',
-        inputLowerName: 'newPlaceLink',
-        buttonText: 'Создать',
-    },
-]
